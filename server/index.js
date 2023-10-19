681792d847300b3fe1ca660e13a6b26b521a3d66
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-10-19 19:07:41 +0530

    initialized project, install starting dependencies

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATION */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
