674a415b3a66f2191ff768c699e030e4b87521c7
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-10-20 21:26:50 +0530

    Completed authentication and login form

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-widht:1000px)");
   return (
-    <div>LoginPage</div>
+    <Box>
+      <Box width="100%" p="1rem 6%" backgroundColor={theme.palette.background.alt} >
+      <Typography
+        fontWeight="bold"
+        fontSize="32px"
+        color="primary"
+      >
+        SocioPedia
+      </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to SocioPedia, the Social Media for Socio Paths
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
   )
 }
 
