4a3f8b632d942f3a93e8b0cee6c7b2ee7b7882c7
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-10-20 08:59:52 +0530

    user and post routes completed

--- server/controller/post.js
+++ server/controller/post.js
@@ -46,3 +46,28 @@ export const getUserPosts = async(req, res)=>{
         res.status(404).json({error: error.message});
     }
 }
+
+export const likePost =  async(req, res)=>{
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const posts = await Posts.findById(id);
+        const isLiked = await posts.likes.get(userId);
+
+        if(isLiked){
+            posts.likes.delete(userId);
+        }else{
+            posts.likes.set(userId , true);
+        }
+
+        const updatedPost = await Posts.findbyIdandUpdate(
+            id,
+            {likes: posts.likes},
+            { new: true},
+        );
+
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
+}
